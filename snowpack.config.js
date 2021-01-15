7ab7de7ac905811a6ae50490fb7300eaf712d220
YogliB <yogiboaron@gmail.com>
2021-01-15 19:57:42 +0200

    clean build folder before each build

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,6 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
+	buildOptions: { clean: true },
 	plugins: ['snowpack-plugin-angular'],
 	routes: [{ match: 'routes', src: '.*', dest: '/index.html' }],
 };
